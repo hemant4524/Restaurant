5b67841fddc6d7ad6fab288a8dae4b469dd68b36
software <hemant4524@gmail.com>
2015-07-29 19:19:07 +0530

    Basic project structure with navigation drawer

--- app/src/main/java/com/htech/restaurant/activity/MainActivity.java
+++ app/src/main/java/com/htech/restaurant/activity/MainActivity.java
@@ -3,12 +3,10 @@ package com.htech.restaurant.activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-import com.htech.restaurant.fragment.SelectTableFragment;
-import com.htech.restaurant.fragment.FragmentIndex;
 import com.htech.restaurant.R;
+import com.htech.restaurant.fragment.FragmentIndex;
+import com.htech.restaurant.fragment.SelectTableFragment;
 
 import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
 
@@ -40,29 +38,6 @@ public class MainActivity extends MaterialNavigationDrawer {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 
     @Override
     protected void onStart() {
